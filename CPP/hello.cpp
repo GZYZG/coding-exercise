0267546630ba380ff08f2fd9c1c4a79cbf48209b
GZYZG <2227423992@qq.com>
2021-09-02 13:22:18 +0800

    刷题-init

---
+++ CPP/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 0;
+    a = 1;
+    a = 2;
+    int b = a;
+    cout << a + b << endl;
+    printf("hello world");
+    return 0;
+}
